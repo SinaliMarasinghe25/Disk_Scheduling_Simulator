e7ca5ce0c48cd472d2bbef4a03e50b67c4e62535
Sinali <sinalimarasingheuni01@gmail.com>
2026-02-26 08:20:28 +0530

    SSTF algorithm added

--- app.py
+++ app.py
@@ -15,3 +15,27 @@ requests = [98, 183, 37, 122, 14, 124, 65, 67]
 seq, mov = fcfs(head, requests)
 print(seq)
 print(mov)
+
+def sstf(head,requests):
+    
+    remainings = requests[:]
+    sequence = []
+    movement = 0
+    current = head
+    
+    while remainings:
+        distances = []
+        for remaining in remainings:
+            distances.append(abs(remaining-current))
+            
+        shortestDistance = min(distances)
+        index = distances.index(shortestDistance)
+        nearest = remainings[index]
+        
+        sequence.append(nearest)
+        movement = movement + abs(current-nearest)
+        current = nearest
+        
+        remainings.remove(nearest)    
+          
+    return sequence,movement
