0a835f3bce0087b1dd4866b7d1fb739478c422b7
Sinali <sinalimarasingheuni01@gmail.com>
2026-03-01 22:13:55 +0530

    SCAN algorithm added

--- app.py
+++ app.py
@@ -39,3 +39,55 @@ def sstf(head,requests):
         remainings.remove(nearest)    
           
     return sequence,movement
+
+def scan(head,requests,disk_size,direction):
+    
+    left = []
+    right = []
+    
+    for request in requests:
+        if request<head:
+            left.append(request)
+        else:
+            right.append(request)
+            
+    left.sort()
+    right.sort()
+    
+    sequence = []
+    movement = 0
+    current = head
+    
+    if direction == "left":
+#(left) is sorted small → big.
+#reversed(left) makes it big → small, so we go left correctly.
+#Example left = [10, 20, 50]
+#reversed = [50, 20, 10]
+        for request in reversed(left):
+            sequence.append(request)
+            movement = movement + abs(current-request)
+            current = request
+            
+        movement = movement + abs(current - 0)
+        current = 0
+        
+        for request in right:
+            sequence.append(request)
+            movement = movement + abs(current-request)
+            current = request
+            
+    else :
+        for request in right:
+            sequence.append(request)
+            movement = movement + abs(current-request)
+            current = request
+            
+        movement = movement + abs(current - (disk_size-1))
+        current = disk_size-1
+        
+        for request in reversed(left):
+            sequence.append(request)
+            movement = movement + abs(current-request)
+            current = request
+            
+    return sequence,movement  
